5e0b2e8198e4ae40b483a870745983ddb356b0c6
Dimas Shofa Gunarso <55543999+Dimasshogun@users.noreply.github.com>
2022-05-26 22:28:48 +0700

    finish part 3

--- Assets/Script/PengolahSoal.cs
+++ Assets/Script/PengolahSoal.cs
@@ -15,9 +15,13 @@ public class PengolahSoal : MonoBehaviour
 
     // komponen UI
     public Text txtSoal, txtOpsiA, txtOpsiB, txtOpsiC, txtOpsiD;
+    bool isHasil;
+    private float durasi;
+    public float durasiPenilaian;
     // Start is called before the first frame update
     void Start()
     {
+        durasi = durasiPenilaian;
         soal = assetSoal.ToString().Split('#');
         soalBag = new string[soal.Length, 6];
         maxSoal = soal.Length;
@@ -60,48 +64,91 @@ public class PengolahSoal : MonoBehaviour
 
         }
     }
+    public GameObject panel;
 
     public void Opsi(string opsiHuruf)
     {
         CheckJawaban(opsiHuruf[0]);
         indexSoal++;
         ambilSoal = true;
-        TampilkanSoal();
-        // if (indexSoal == maxSoal - 1)
-        // {
-        //     isHasil = true;
-        // }
-        // else
-        // {
-        //     indexSoal++;
-        //     ambilSoal = true;
-        // }
-        // panel.SetActive(true);
+        // TampilkanSoal();
+        if (indexSoal == maxSoal - 1)
+        {
+            isHasil = true;
+        }
+        else
+        {
+            indexSoal++;
+            ambilSoal = true;
+        }
+        panel.SetActive(true);
     }
 
+    public Text txtPenilaian;
     private void CheckJawaban(char huruf)
     {
         string penilaian;
 
         if (huruf.Equals(kunciJ))
         {
-            print("benar");
-            // penilaian = "Benar!";
+            // print("benar");
+            penilaian = "Benar!";
             // jwbBenar++;
         }
         else
         {
-            print("salah");
-            // penilaian = "Salah!";
+            // print("salah");
+            penilaian = "Salah!";
             // jwbSalah++;
         }
-        // txtPenilaian.text = penilaian;
+        txtPenilaian.text = penilaian;
 
 
     }
     // Update is called once per frame
     void Update()
     {
+        if (panel.activeSelf)
+        {
+            durasiPenilaian -= Time.deltaTime;
 
+            if (durasiPenilaian <= 0)
+            {
+                panel.SetActive(false);
+                durasiPenilaian = durasi;
+
+                TampilkanSoal();
+            }
+
+            // if (isHasil)
+            // {
+            //     imgPenilaian.SetActive(true);
+            //     imgHasil.SetActive(false);
+
+            //     if (durasiPenilaian <= 0)
+            //     {
+            //         txtHasil.text = "Jumlah benar: " + jwbBenar + "\nJumlah Salah: " + jwbSalah + "\n\nNilai: " + HitungNilai();
+
+            //         imgPenilaian.SetActive(false);
+            //         imgHasil.SetActive(true);
+
+            //         durasiPenilaian = 0;
+            //     }
+            // }
+            // else
+            // {
+            //     imgPenilaian.SetActive(true);
+            //     imgHasil.SetActive(false);
+
+            //     if (durasiPenilaian <= 0)
+            //     {
+            //         panel.SetActive(false);
+            //         durasiPenilaian = durasi;
+
+            //         TampilkanSoal();
+            //     }
+            // }
+        }
     }
 }
+
